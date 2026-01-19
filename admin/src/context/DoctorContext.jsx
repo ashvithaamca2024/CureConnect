a4ff535959254d104dda20d501389a4ffa63d5fe
ASHVITHA A <ashwithaaashe2401@gmail.com>
2026-01-19 23:14:34 +0530

    Update backend URL in DoctorContext
    Changed backend URL to a fixed value for the DoctorContext.

--- admin/src/context/DoctorContext.jsx
+++ admin/src/context/DoctorContext.jsx
@@ -4,7 +4,7 @@ import { toast } from "react-toastify";
 
 export const DoctorContext = createContext();
 const DoctorContextProvider = (props) => {
-  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const backendUrl = "https://cureconnect-backend-u3oa.onrender.com";
 
   const [dToken, setDToken] = useState(
     localStorage.getItem("dToken") ? localStorage.getItem("dToken") : ""
