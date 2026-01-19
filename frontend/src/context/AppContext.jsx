71a56314e2cb2b03267a537b15fe3d291586304c
ASHVITHA A <ashwithaaashe2401@gmail.com>
2026-01-19 23:13:09 +0530

    Update AppContext.jsx

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -6,7 +6,7 @@ export const AppContext = createContext();
 
 const AppContextProvider = (props) => {
   const currencySymbol = "₹";
-  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const backendUrl = "https://cureconnect-backend-u3oa.onrender.com";
 
   const [doctors, setDoctors] = useState([]);
   const [token, setToken] = useState(
